3cf0f19db9ddcd583ca74cfef06d4aa416d257e2
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-29 13:40:22 -0400

    finished today's assignment

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  const electionList = document.querySelector('#election_list');
+  axios.get("https://bb-election-api.herokuapp.com/")
+    .then((resp) => {
+      resp.data.candidates.forEach((element) => {
+        let newLi = document.createElement('li');
+        newLi.innerText = "Name: " + element.name + "\nVotes: " + element.votes;
+        electionList.appendChild(newLi);
+      })
+    })
 });
