db04c838698ce5836796850f668dd0a45e1fb17d
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-01 14:10:33 -0400

    finished the exercise

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const electionList = document.querySelector('#election_list');
-  axios.get("https://bb-election-api.herokuapp.com/")
+  function newPage() {
+    axios.get("https://bb-election-api.herokuapp.com/")
     .then((resp) => {
+      electionList.innerText = ''
       resp.data.candidates.forEach((element) => {
         let newLi = document.createElement('li');
         const newForm = document.createElement('form');
@@ -9,6 +11,14 @@ document.addEventListener("DOMContentLoaded", function() {
         newForm.action = "https://bb-election-api.herokuapp.com/vote";
         const newButton = document.createElement('button');
         newButton.innerText = `Vote for ${element.name}`;
+        newButton.id = element.id;
+        newButton.addEventListener("click", event => {
+          event.preventDefault();
+          axios.post("https://bb-election-api.herokuapp.com/vote", {
+            id: event.target.id
+          })
+            .then(() => {newPage()})
+        })
         const newInput = document.createElement('input');
         newInput.name = "id";
         newInput.value = element.id;
@@ -16,8 +26,15 @@ document.addEventListener("DOMContentLoaded", function() {
         newForm.appendChild(newInput);
         newForm.appendChild(newButton);
         newLi.innerText = "Name: " + element.name + "\nVotes: " + element.votes;
+        newLi.classList = element.id;
         newLi.appendChild(newForm);
         electionList.appendChild(newLi);
       })
     })
+  }
+  newPage()
+  refresh.addEventListener('click', () => {
+    console.log('sup')
+    newPage()
+  })
 });
