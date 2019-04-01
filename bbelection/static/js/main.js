8b076da2f0d7f3b2f385bb9a92de52ba2b90e6f3
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-01 13:36:15 -0400

    added synchronous voting

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -4,7 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     .then((resp) => {
       resp.data.candidates.forEach((element) => {
         let newLi = document.createElement('li');
+        const newForm = document.createElement('form');
+        newForm.method = "POST";
+        newForm.action = "https://bb-election-api.herokuapp.com/vote";
+        const newButton = document.createElement('button');
+        newButton.innerText = `Vote for ${element.name}`;
+        const newInput = document.createElement('input');
+        newInput.name = "id";
+        newInput.value = element.id;
+        newInput.setAttribute("type", "hidden");
+        newForm.appendChild(newInput);
+        newForm.appendChild(newButton);
         newLi.innerText = "Name: " + element.name + "\nVotes: " + element.votes;
+        newLi.appendChild(newForm);
         electionList.appendChild(newLi);
       })
     })
